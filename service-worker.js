99b02834751c8318c399112fc523a1be3a7886f7
andyhung6969 <andyhung0609@gmail.com>
2026-07-09 15:32:09 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const C='fire-os-3-preview-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./assets/css/app.css','./assets/js/app.js']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
